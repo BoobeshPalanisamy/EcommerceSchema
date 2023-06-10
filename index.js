2b2e39d540fd6698244352952b23d60aa21683b1
kumuthac23 <kumuthac23@gmail.com>
2023-06-10 14:46:16 +0530

    fixed fetch all products by category it

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const ProductModel = require("./Models/Product");
-const SignupModel = require('./Models/User');
+const SignupModel = require("./Models/User");
 const CategoryModel = require("./Models/CategoryTest");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
 
 main().catch((err) => console.log(err));
 
@@ -29,9 +28,7 @@ mongoose.connection.on("connected", () => {
 //   })
 // );
 
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -78,12 +75,12 @@ app.get("/getAllProductsByCategory", async (req, res) => {
               input: { $slice: ["$Allproducts", 10] },
               as: "product",
               in: {
-                title: "$$product.title",
                 // image: { $arrayElemAt: ["$$product.image", 0] },
-                price: "$$product.price",
-                discount: "$$product.discount",
+                title: "$$product.title",
                 posterUrl: "$$product.posterURL",
-                materialType: "$$product.materialType",
+                price: "$$product.price",
+                code : "$$product.productCode",
+                discount: "$$product.discount",
               },
             },
           },
@@ -105,12 +102,13 @@ app.get("/getAllProductsByCategory", async (req, res) => {
 
 app.get("/fetchProductsByCategory/:categoryId", async (req, res) => {
   try {
-    const categoaryId = req.params.categoryId;
-    const id = new mongoose.Types.ObjectId(categoaryId);
-    const products = await ProductModel.aggregate([
+    const categoryId = req.params.categoryId;
+
+    const _categoryId = new mongoose.Types.ObjectId(categoryId);
+    const products = await CategoryModel.aggregate([
       {
         $match: {
-          _id: id,
+          _id: _categoryId,
         },
       },
       {
@@ -122,32 +120,43 @@ app.get("/fetchProductsByCategory/:categoryId", async (req, res) => {
         },
       },
       {
-        $project: {
-          Allproducts: {
+        $addFields: {
+          products: {
             $map: {
               input: "$Allproducts",
               as: "product",
               in: {
                 title: "$$product.title",
-                posterUrl: "$$product.image",
+                posterUrl: "$$product.posterURL",
                 price: "$$product.price",
-                color: "$$product.color",
-                materialType: "$$product.materialType",
+                code : "$$product.productCode",
+                discount: "$$product.discount",
               },
             },
           },
         },
       },
+      {
+        $project: {
+          name: 1,
+          image: 1,
+          products: 1,
+        },
+      },
     ]);
+
+    // const products = await ProductModel.find(
+    //   { category: _categoryId },
+    //   { title: 1, posterURL: 1, price: 1, productCode: 1 }
+    // ).populate("category", "name image");
     res.json(products);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch products" });
+    res.status(500).json(error.message);
   }
 });
 
-
-
 // Get all category
+
 app.get("/fetchCategory", async (req, res) => {
   var course = await CategoryModel.find();
   res.json(course);
@@ -179,157 +188,156 @@ app.get("/fetchProductByID/:productId", async (req, res) => {
   }
 });
 
-
 // Signup & Login using JWT(Json Web Token) authorization
 
 const authorization = (req, res, next) => {
-    const token = req.cookies.access_token;
-    console.log(token)
-    if (!token) {
-        return res.sendStatus(403);
-    }
-    try {
-        const data = jwt.verify(token, "mystery");
-        req.id = data.userId
-        req.email = data.email;
-        return next();
-    } catch {
-        return res.sendStatus(403);
-    }
+  const token = req.cookies.access_token;
+  console.log(token);
+  if (!token) {
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, "mystery");
+    req.id = data.userId;
+    req.email = data.email;
+    return next();
+  } catch {
+    return res.sendStatus(403);
+  }
 };
-
-
 
 // SignUp
 
 app.post("/signup", async (req, res) => {
-    let { name, email, password } = req.body;
-    name = name.trim();
-    email = email.trim();
-    password = password.trim();
+  let { name, email, password } = req.body;
+  name = name.trim();
+  email = email.trim();
+  password = password.trim();
 
-    if (name === "" || email === "" || password === "") {
+  if (name === "" || email === "" || password === "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty input fields!",
+    });
+  } else if (!/^[a-zA-Z\s]+$/.test(name)) {
+    res.json({
+      status: "FAILED",
+      message: "Invalid name format",
+    });
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    res.json({
+      status: "FAILED",
+      message: "Invalid email format",
+    });
+  } else if (password.length < 8) {
+    res.json({
+      status: "FAILED",
+      message: "Password is too short!",
+    });
+  } else {
+    try {
+      // Check if the user already exists
+      const existingUser = await SignupModel.findOne({ email });
+      if (existingUser) {
         res.json({
-            status: "FAILED",
-            message: "Empty input fields!",
+          status: "FAILED",
+          message: "User with provided email already exists",
         });
-    } else if (!/^[a-zA-Z\s]+$/.test(name)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid name format",
+      } else {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new SignupModel({
+          name,
+          email,
+          password: hashedPassword,
         });
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid email format",
-        });
-    } else if (password.length < 8) {
-        res.json({
-            status: "FAILED",
-            message: "Password is too short!",
-        });
-    } else {
-        try {
-            // Check if the user already exists
-            const existingUser = await SignupModel.findOne({ email });
-            if (existingUser) {
-                res.json({
-                    status: "FAILED",
-                    message: "User with provided email already exists",
-                });
-            } else {
-                const saltRounds = 10;
-                const hashedPassword = await bcrypt.hash(password, saltRounds);
-                const newUser = new SignupModel({
-                    name,
-                    email,
-                    password: hashedPassword,
-                });
-                const savedUser = await newUser.save();
-                // Generate JWT token
-                const token = jwt.sign({ userId: savedUser._id, email: savedUser.email }, 'mystery');
-                res
-                    .cookie("access_token", token, {
-                        httpOnly: true,
-                        secure: false,
-                    })
-                    .json({
-                        status: "200",
-                        message: "Signup Successful",
-                        data: savedUser,
-                    });
-            }
-        } catch (err) {
-            console.error(err);
-            res.json({
-                status: "FAILED",
-                message: "An error occurred while signing up!",
-            });
-        }
+        const savedUser = await newUser.save();
+        // Generate JWT token
+        const token = jwt.sign(
+          { userId: savedUser._id, email: savedUser.email },
+          "mystery"
+        );
+        res
+          .cookie("access_token", token, {
+            httpOnly: true,
+            secure: false,
+          })
+          .json({
+            status: "200",
+            message: "Signup Successful",
+            data: savedUser,
+          });
+      }
+    } catch (err) {
+      console.error(err);
+      res.json({
+        status: "FAILED",
+        message: "An error occurred while signing up!",
+      });
     }
+  }
 });
 
-// Signin 
+// Signin
 
-app.post('/login', async (req, res) => {
-    let { email, password } = req.body;
-    email = email.trim();
-    password = password.trim();
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  email = email.trim();
+  password = password.trim();
 
-    if (email === "" || password === "") {
-        res.json({
-            status: "FAILED",
-            message: "Empty credentials supplied"
+  if (email === "" || password === "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty credentials supplied",
+    });
+  } else {
+    try {
+      // Check if user exists
+      const user = await SignupModel.findOne({ email });
+      if (!user) {
+        return res.json({
+          status: "FAILED",
+          message: "Invalid credentials entered!",
         });
-    } else {
-        try {
-            // Check if user exists
-            const user = await SignupModel.findOne({ email });
-            if (!user) {
-                return res.json({
-                    status: "FAILED",
-                    message: "Invalid credentials entered!"
-                });
-            }
+      }
 
-            // Compare passwords
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-                return res.json({
-                    status: "FAILED",
-                    message: "Invalid password entered!"
-                });
-            }
+      // Compare passwords
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.json({
+          status: "FAILED",
+          message: "Invalid password entered!",
+        });
+      }
 
-            // Generate JWT token
-            const token = jwt.sign({ userId: user._id, email: user.email }, 'mystery');
-            res.cookie("access_token", token, {
-                httpOnly: true,
-                // secure: process.env.NODE_ENV === "production",
-            });
+      // Generate JWT token
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        "mystery"
+      );
+      res.cookie("access_token", token, {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === "production",
+      });
 
-            res.json({
-                status: "200",
-                message: "Signin successful",
-                data: user
-            });
-        } catch (err) {
-            console.error(err);
-            res.json({
-                status: "FAILED",
-                message: "An error occurred while signing in!"
-            });
-        }
+      res.json({
+        status: "200",
+        message: "Signin successful",
+        data: user,
+      });
+    } catch (err) {
+      console.error(err);
+      res.json({
+        status: "FAILED",
+        message: "An error occurred while signing in!",
+      });
     }
+  }
 });
-
 
 app.get("/protected", authorization, (req, res) => {
-    return res.json({ user: { id: req.id, email: req.email } });
+  return res.json({ user: { id: req.id, email: req.email } });
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
