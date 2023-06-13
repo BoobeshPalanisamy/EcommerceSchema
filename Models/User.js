0d60108451b86fc48bd4a6b83c2f20eb20d5a77d
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-06-13 16:54:29 +0530

    addressapi

--- Models/User.js
+++ Models/User.js
@@ -1,21 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const signupSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
+//   PhoneNumber: {
+//     type: Number,
+//     required: true,
+//   },
+  ShippingDetails: [
+    {
+      Name: {
         type: String,
         required: true,
-        unique: true
-    },
-    password: {
+      },
+      PhoneNumber: {
+        type: Number,
+        required: true,
+      },
+      AlternatePhone: {
+        type: Number,
+        required: true,
+      },
+      Locality: {
         type: String,
-        required: true
-    }
+        required: true,
+      },
+      Address: {
+        type: String,
+        required: true,
+      },
+      City: {
+        type: String,
+        required: true,
+      },
+      Pincode: {
+        type: Number,
+        required: true,
+      },
+      State: {
+        type: String,
+        required: true,
+      },
+      Landmark: {
+        type: String,
+        required: true,
+      },
+      AddressType: {
+        type: String,
+        required: true,
+      },
+      main: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
 });
 
-const SignupModel = mongoose.model('Signup', signupSchema);
+const SignupModel = mongoose.model("Register", signupSchema);
 
 module.exports = SignupModel;
--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const ProductModel = require("./Models/Product");
-const SignupModel = require('./Models/User');
+const SignupModel = require("./Models/User");
 const CategoryModel = require("./Models/CategoryTest");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
 
 main().catch((err) => console.log(err));
 
@@ -24,13 +23,31 @@ mongoose.connection.on("connected", () => {
 
 app.use(
   cors({
-    credentials: "true",
+    credentials: true,
     origin: "http://localhost:5173",
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
+
+// Signup & Login using JWT(Json Web Token) authorization
+
+const authorization = (req, res, next) => {
+  const token = req.cookies.access_token;
+  console.log(token);
+  if (!token) {
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, "mystery");
+    req.id = data.userId;
+    req.email = data.email;
+    return next();
+  } catch {
+    return res.sendStatus(403);
+  }
+};
 
 // This API is for Category
 app.post("/createCategory", async (req, res) => {
@@ -141,10 +158,8 @@ app.get("/fetchProductsByCategory/:categoryId", async (req, res) => {
   }
 });
 
-
-
 // Get all category
-app.get("/fetchCategory", async (req, res) => {
+app.get("/fetchCategory", authorization, async (req, res) => {
   var course = await CategoryModel.find();
   res.json(course);
 });
@@ -175,157 +190,70 @@ app.get("/fetchProductByID/:productId", async (req, res) => {
   }
 });
 
+// RegisterAddress
+app.post("/registeraddress", async (req, res) => {
+  try {
+    // const {
+    //   Name,
+    //   PhoneNumber,
+    //   AlternatePhone,
+    //   Locality,
+    //   Address,
+    //   City,
+    //   Pincode,
+    //   State,
+    //   Landmark,
+    //   AddressType,
+    // } = req.body;
 
-// Signup & Login using JWT(Json Web Token) authorization
+    const newSignup = new SignupModel({
+      // Name,
+      // PhoneNumber,
+      // AlternatePhone,
+      // Locality,
+      // Address,
+      // City,
+      // Pincode,
+      // State,
+      // Landmark,
+      // AddressType,
+      ...req.body,
+    });
 
-const authorization = (req, res, next) => {
-    const token = req.cookies.access_token;
-    console.log(token)
-    if (!token) {
-        return res.sendStatus(403);
-    }
-    try {
-        const data = jwt.verify(token, "mystery");
-        req.id = data.userId
-        req.email = data.email;
-        return next();
-    } catch {
-        return res.sendStatus(403);
-    }
-};
+    await newSignup.save();
 
+    res.status(201).json({ message: "Registration successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred during registration" });
+  }
+});
 
+// Get user by ID for address
+app.get("/user/:id", async (req, res) => {
+  const userId = req.params.id;
 
-// SignUp
-
-app.post("/signup", async (req, res) => {
-    let { name, email, password } = req.body;
-    name = name.trim();
-    email = email.trim();
-    password = password.trim();
-
-    if (name === "" || email === "" || password === "") {
-        res.json({
-            status: "FAILED",
-            message: "Empty input fields!",
-        });
-    } else if (!/^[a-zA-Z\s]+$/.test(name)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid name format",
-        });
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid email format",
-        });
-    } else if (password.length < 8) {
-        res.json({
-            status: "FAILED",
-            message: "Password is too short!",
-        });
+  try {
+    const user = await SignupModel.findById(userId);
+    if (user) {
+      res.json({
+        status: "SUCCESS",
+        data: user,
+      });
     } else {
-        try {
-            // Check if the user already exists
-            const existingUser = await SignupModel.findOne({ email });
-            if (existingUser) {
-                res.json({
-                    status: "FAILED",
-                    message: "User with provided email already exists",
-                });
-            } else {
-                const saltRounds = 10;
-                const hashedPassword = await bcrypt.hash(password, saltRounds);
-                const newUser = new SignupModel({
-                    name,
-                    email,
-                    password: hashedPassword,
-                });
-                const savedUser = await newUser.save();
-                // Generate JWT token
-                const token = jwt.sign({ userId: savedUser._id, email: savedUser.email }, 'mystery');
-                res
-                    .cookie("access_token", token, {
-                        httpOnly: true,
-                        secure: false,
-                    })
-                    .json({
-                        status: "200",
-                        message: "Signup Successful",
-                        data: savedUser,
-                    });
-            }
-        } catch (err) {
-            console.error(err);
-            res.json({
-                status: "FAILED",
-                message: "An error occurred while signing up!",
-            });
-        }
+      res.json({
+        status: "FAILED",
+        message: "User not found",
+      });
     }
+  } catch (err) {
+    console.error(err);
+    res.json({
+      status: "FAILED",
+      message: "An error occurred while fetching the user",
+    });
+  }
 });
-
-// Signin 
-
-app.post('/login', async (req, res) => {
-    let { email, password } = req.body;
-    email = email.trim();
-    password = password.trim();
-
-    if (email === "" || password === "") {
-        res.json({
-            status: "FAILED",
-            message: "Empty credentials supplied"
-        });
-    } else {
-        try {
-            // Check if user exists
-            const user = await SignupModel.findOne({ email });
-            if (!user) {
-                return res.json({
-                    status: "FAILED",
-                    message: "Invalid credentials entered!"
-                });
-            }
-
-            // Compare passwords
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-                return res.json({
-                    status: "FAILED",
-                    message: "Invalid password entered!"
-                });
-            }
-
-            // Generate JWT token
-            const token = jwt.sign({ userId: user._id, email: user.email }, 'mystery');
-            res.cookie("access_token", token, {
-                httpOnly: true,
-                // secure: process.env.NODE_ENV === "production",
-            });
-
-            res.json({
-                status: "200",
-                message: "Signin successful",
-                data: user
-            });
-        } catch (err) {
-            console.error(err);
-            res.json({
-                status: "FAILED",
-                message: "An error occurred while signing in!"
-            });
-        }
-    }
-});
-
-
-app.get("/protected", authorization, (req, res) => {
-    return res.json({ user: { id: req.id, email: req.email } });
-});
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
