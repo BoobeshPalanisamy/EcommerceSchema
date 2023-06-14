cfff24b05ad3a82a2f055bc67eca3d4e49ab3899
praveensumathi <praveensumathi98@gmail.com>
2023-06-14 07:27:53 +0530

    added payment api for test

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const CategoryModel = require("./Models/CategoryTest");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+var crypto = require("crypto");
+const Razorpay = require("razorpay");
 
-main().catch((err) => console.log(err));
+//main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_CONNECTION);
@@ -440,6 +442,55 @@ app.post("/login", async (req, res) => {
 
 app.get("/protected", authorization, (req, res) => {
   return res.json({ user: { id: req.id, email: req.email } });
+});
+
+//Get Order details from the Razorpay
+app.post("/orders", async (req, res) => {
+  try {
+    const instance = new Razorpay({
+      key_id: process.env.KEY_ID,
+      key_secret: process.env.KEY_SECRET,
+    });
+
+    const options = {
+      amount: req.body.amount * 100,
+      currency: "INR",
+      receipt: crypto.randomBytes(10).toString("hex"),
+    };
+
+    instance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+      }
+
+      res.status(200).json(order);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/verify", async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSign = crypto
+      .createHmac("sha256", process.env.KEY_SECRET)
+      .update(sign.toString())
+      .digest("hex");
+    if (razorpay_signature === expectedSign) {
+      return res.status(200).json({ message: "Payment verified successfully" });
+    } else {
+      return res.status(400).json({ message: "Invalid signature sent!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
