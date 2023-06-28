309acc1435a23a2daf7b829b88888d9d3e9c1d40
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-06-28 21:34:00 +0530

    resetpassword

--- Models/User.js
+++ Models/User.js
@@ -1,60 +1,83 @@
+// const mongoose = require("mongoose");
+
+// const signupSchema = new mongoose.Schema({
+// //   PhoneNumber: {
+// //     type: Number,
+// //     required: true,
+// //   },
+//   ShippingDetails: [
+//     {
+//       Name: {
+//         type: String,
+//         required: true,
+//       },
+//       PhoneNumber: {
+//         type: Number,
+//         required: true,
+//       },
+//       AlternatePhone: {
+//         type: Number,
+//         required: true,
+//       },
+//       Locality: {
+//         type: String,
+//         required: true,
+//       },
+//       Address: {
+//         type: String,
+//         required: true,
+//       },
+//       City: {
+//         type: String,
+//         required: true,
+//       },
+//       Pincode: {
+//         type: Number,
+//         required: true,
+//       },
+//       State: {
+//         type: String,
+//         required: true,
+//       },
+//       Landmark: {
+//         type: String,
+//         required: true,
+//       },
+//       AddressType: {
+//         type: String,
+//         required: true,
+//       },
+//       main: {
+//         type: Boolean,
+//         default: false,
+//       },
+//     },
+//   ],
+// });
+
+// const SignupModel = mongoose.model("Register", signupSchema);
+
+// module.exports = SignupModel;
+
 const mongoose = require("mongoose");
 
-const signupSchema = new mongoose.Schema({
-//   PhoneNumber: {
-//     type: Number,
-//     required: true,
-//   },
-  ShippingDetails: [
-    {
-      Name: {
-        type: String,
-        required: true,
-      },
-      PhoneNumber: {
-        type: Number,
-        required: true,
-      },
-      AlternatePhone: {
-        type: Number,
-        required: true,
-      },
-      Locality: {
-        type: String,
-        required: true,
-      },
-      Address: {
-        type: String,
-        required: true,
-      },
-      City: {
-        type: String,
-        required: true,
-      },
-      Pincode: {
-        type: Number,
-        required: true,
-      },
-      State: {
-        type: String,
-        required: true,
-      },
-      Landmark: {
-        type: String,
-        required: true,
-      },
-      AddressType: {
-        type: String,
-        required: true,
-      },
-      main: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  ],
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phonenumber: {
+    type: Number,
+    required: true,
+  },
+  email: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  isResetLinkUsed:String
 });
 
-const SignupModel = mongoose.model("Register", signupSchema);
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = SignupModel;
+module.exports = UserModel;
