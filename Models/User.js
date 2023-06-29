b5f0ab09823e85e4594766af74f0430977fc96ad
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-06-29 11:36:32 +0530

    Schemachange

--- Models/User.js
+++ Models/User.js
@@ -1,64 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const signupSchema = new mongoose.Schema({
-// //   PhoneNumber: {
-// //     type: Number,
-// //     required: true,
-// //   },
-//   ShippingDetails: [
-//     {
-//       Name: {
-//         type: String,
-//         required: true,
-//       },
-//       PhoneNumber: {
-//         type: Number,
-//         required: true,
-//       },
-//       AlternatePhone: {
-//         type: Number,
-//         required: true,
-//       },
-//       Locality: {
-//         type: String,
-//         required: true,
-//       },
-//       Address: {
-//         type: String,
-//         required: true,
-//       },
-//       City: {
-//         type: String,
-//         required: true,
-//       },
-//       Pincode: {
-//         type: Number,
-//         required: true,
-//       },
-//       State: {
-//         type: String,
-//         required: true,
-//       },
-//       Landmark: {
-//         type: String,
-//         required: true,
-//       },
-//       AddressType: {
-//         type: String,
-//         required: true,
-//       },
-//       main: {
-//         type: Boolean,
-//         default: false,
-//       },
-//     },
-//   ],
-// });
-
-// const SignupModel = mongoose.model("Register", signupSchema);
-
-// module.exports = SignupModel;
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -75,7 +14,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  isResetLinkUsed:String
+  isResetLinkUsed: String,
 });
 
 const UserModel = mongoose.model("User", userSchema);
